d1e2229ff38f67dec83b23ae706e3b9f842b8464
moha1747 <mohamud.abbaas14@gmail.com>
2023-11-12 23:13:17 -0600

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Other configuration options...
+    // Add the following line to enable static HTML export
+    exportPathMap: () => ({ '/': { page: '/' } }),
+    exportTrailingSlash: true, // optional, depending on your needs
+    output: 'export', // Set the output option to 'export'
+  };
+  
+  module.exports = nextConfig;
+  
