1bb2d30a585c5f136e0957f05671ed88dc55003b
moha1747 <mohamud.abbaas14@gmail.com>
2023-11-13 00:21:44 -0600

    fix deploy

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@
 const nextConfig = {
     // Other configuration options...
     // Add the following line to enable static HTML export
-    exportPathMap: () => ({ '/': { page: '/' } }),
-    exportTrailingSlash: true, // optional, depending on your needs
+    async generateStaticParams() {
+        // Define your static paths here
+        return [
+          { params: { id: '1' } },
+          { params: { id: '2' } },
+          // Add more paths as needed
+        ];
+      },
+
     output: 'export', // Set the output option to 'export'
   };
   
